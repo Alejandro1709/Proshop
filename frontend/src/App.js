fb90dbc85c769d623684e6cfda53bdd736904eff
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2020-10-27 17:29:18 -0500

    ADD: Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Home />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
